af44a900c1939dca2548930e8497760f49264df8
Jessica White <jessica2714white@gmail.com>
2021-11-12 07:02:40 -0600

    Post router

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,3 +2,21 @@ const router = require("express").Router();
 const { Comment, User, Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+// post router
+router.post("/", withAuth, (req, res) => {
+    const body = req.body;
+    console.log(req.session.userId);
+    Post.create({ ...body, userId: req.session.userId })
+        .then(newPost => {
+            res.json(newPost);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+
+// put router
+
+
+// delete router
