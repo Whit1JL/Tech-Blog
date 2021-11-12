dea4a64582cafa76d313482dfe1896735d39bf3b
Jessica White <jessica2714white@gmail.com>
2021-11-12 07:17:55 -0600

    Get one post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,4 +17,26 @@ router.get("/", (req, res) => {
 });
 
 // get one post 
+router.get("/post/:id", (req, res) => {
+    Post.findByPk(req.params.id, {
+        include: [
+            User,
+            {
+                model: Comment,
+                include: [User],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            if (dbPostData) {
+                const post = dbPostData.get({ plain: true });
 
+                res.render("single-post", { post });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
