b64fe7bd3b089dd19079698c53ea0498efd1194f
Jessica White <jessica2714white@gmail.com>
2021-11-12 07:06:58 -0600

    Route for log out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,3 +50,16 @@ router.post("/login", (req, res) => {
         });
     });
 });
+
+// post router for logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
