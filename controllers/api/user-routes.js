b5820aca1b3cb8381379b9031f738283b4f03488
Jessica White <jessica2714white@gmail.com>
2021-11-12 07:05:36 -0600

    User create

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,24 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// post router
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.userId = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
