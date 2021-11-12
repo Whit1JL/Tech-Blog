2a2ff7408dc9e769a067fe3cabb56c48729cd921
Jessica White <jessica2714white@gmail.com>
2021-11-12 07:20:43 -0600

    Find all posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,25 @@ const router = require("express").Router();
 const { Post } = require("../models/");
 const withAuth = require("../utils/auth");
 
+// find all posts
+router.get("/", withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            userId: req.session.userId
+        }
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+            res.render("all-posts-admin", {
+                layout: "dashboard",
+                posts
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect("login");
+        });
+});
+
+// get new post
